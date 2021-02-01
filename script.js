c08557f54ce40a41cc94810a5e358a9edc42cd20
Kai Curtis <kai6curtis@gmail.com>
2021-01-31 22:30:22 -0700

    added new homework and project repo button links

--- script.js
+++ script.js
@@ -4,53 +4,72 @@ $(document).ready(function () {
     // $(".embed").hide();
     $("#planner").hide();
     $("#homeWork").hide();
+    $("#whatsnotapp").hide();
     $(".project1").hide();
+    $(".project2").hide();
     $(".homeWork1").hide();
     $(".homeWork2").hide();
+    $(".homeWork3").hide();
   });
 
   $(".pictures").on("click", () => {
     $(".funPics").toggle();
     $(".embed").hide();
     $("#planner").hide();
+    $("#whatsnotapp").hide();
     $("#homeWork").hide();
     $(".project1").hide();
+    $(".project2").hide();
     $(".homeWork1").hide();
     $(".homeWork2").hide();
+    $(".homeWork3").hide();
   });
 
   $(".resume").on("click", () => {
     $(".embed").toggle();
     $(".funPics").hide();
     $("#planner").hide();
+    $("#whatsnotapp").hide();
     $("#homeWork").hide();
     $(".project1").hide();
+    $(".project2").hide();
     $(".homeWork1").hide();
     $(".homeWork2").hide();
+    $(".homeWork3").hide();
   });
 
   $(".project").on("click", () => {
     $("#planner").toggle();
+    $("#whatsnotapp").toggle();
     $(".funPics").hide();
     $(".embed").hide();
     $("#homeWork").hide();
     $(".project1").toggle();
+    $(".project2").toggle();
     $(".homeWork1").hide();
     $(".homeWork2").hide();
+    $(".homeWork3").hide();
   });
 
   $(".homeWork").on("click", () => {
     $("#homeWork").toggle();
     $(".homeWork1").toggle();
     $(".homeWork2").toggle();
+    $(".homeWork3").toggle();
     $(".funPics").hide();
     $(".embed").hide();
     $("#planner").hide();
+    $("#whatsnotapp").hide();
     $(".project1").hide();
+    $(".project2").hide();
   });
 
   $(".project1").on("click", () => {
-    window.open("https://github.com/JonDnv/FamilyPlanner");
+    window.open("https://github.com/kaiasahite/FamilyPlanner");
+  });
+
+  $(".project2").on("click", () => {
+    window.open("https://github.com/kaiasahite/whatsnotapp-messaging");
   });
 
   $(".homeWork1").on("click", () => {
@@ -59,5 +78,9 @@ $(document).ready(function () {
 
   $(".homeWork2").on("click", () => {
     window.open("https://github.com/kaiasahite/skateboard-quiz");
+  });
+
+  $(".homeWork3").on("click", () => {
+    window.open("https://github.com/kaiasahite/password-generator");
   });
 });
